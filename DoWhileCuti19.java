430bdfca17fcbcc5bbec2be6f93dd1019e25a2db
mrfauzii <reishifauzi24@gmail.com>
2023-10-12 16:40:08 +0700

    menghilangkan break

--- DoWhileCuti19.java
+++ DoWhileCuti19.java
@@ -23,7 +23,6 @@ public class DoWhileCuti19 {
                         System.out.println("Sisa jatah cuti: "+ jatahCuti);
                     } else {
                         System.out.println("Sisa jatah cuti Anda tidak mencukupi");
-                        break;
 
                     }
                 }
